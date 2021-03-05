dc5c1e7f3d3be4a94fb2d21cf07d5d883ef9184c
Nechama Turin <nturin398@gmail.com>
2021-03-05 00:39:53 -0500

    With delete button working.

--- PeopleList/wwwroot/js/AddPeople.js
+++ PeopleList/wwwroot/js/AddPeople.js
@@ -3,10 +3,9 @@
     let count = 1
 
     $("#add-rows").on('click', function () {
-        $("#ppl-rows").append(`<div class="row" style="margin-bottom: 10px;">
-
+        $("#ppl-rows").append(`<div class="row" id="form-control"  style="margin-bottom: 10px;">
                             <div class="col-md-4">
-                                <input class="form-control" type="text" name="people[${count}].firstname" placeholder="First Name" />
+                                <input class="form-control " type="text" name="people[${count}].firstname" placeholder="First Name" />
                             </div>
                             <div class="col-md-4">
                                 <input class="form-control" type="text" name="people[${count}].lastname" placeholder="Last Name" />
@@ -16,7 +15,12 @@
                             </div>
                         </div>`);
         count++;
+        console.log({ count });
     });
 
+    $("#delete-rows").on('click', function () {
+        console.log('here')
+        $("#form-control").remove();
+    });
   
 })
